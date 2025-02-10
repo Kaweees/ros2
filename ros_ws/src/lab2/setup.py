68d1047851e548ffa47f91cf3dcc4a835d73d519
Miguel Villa Floran <miguel.villafloran@gmail.com>
2025-02-09 20:26:00 -0800

    lab2

---
+++ ros_ws/src/lab2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+package_name = 'lab2'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Miguel Villa Floran',
+    maintainer_email='miguel.villa.floran@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'talker = src.talker:main',
+            'listener = src.listener:main',
+            'squared = src.squared:main'
+        ],
+    },
+)
